a8594dd65e2447e05b7091a91360ab86acf6566b
milesbliss0022 <117626287+milesbliss0022@users.noreply.github.com>
2022-11-07 18:50:50 -0800

    Added new  files

---
+++ app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('#mobile-menu')
+const menu = document.querySelector('.navbar_menu')
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('is-active');
+});
